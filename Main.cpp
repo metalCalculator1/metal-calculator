0e8af36173f547a4d49858b60e2f5650168ef774
OraCL12 <pelo-pelo@ukr.net>
2024-02-06 15:45:42 +0200

    base responsive interface of setting page

--- Main.cpp
+++ Main.cpp
@@ -9,6 +9,7 @@ using namespace System::Windows::Forms;
 void main(array<String^>^ args) {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
-	MetalCalculator::MainMenu form;
+	//MetalCalculator::MainMenu form;
+	MetalCalculator::SettingsMenu form;
 	Application::Run(% form);
 }
